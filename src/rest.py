d3238202b7ea0bc71e2f5b1c01e052de667303bf
Stavros Stavrou <45168559+stavros2@users.noreply.github.com>
2020-03-18 15:33:10 +0200

    Small Update

--- src/rest.py
+++ src/rest.py
@@ -4,46 +4,45 @@ from flask_cors import CORS
 
 
 import block
-#import node
+import node
 import blockchain
 import wallet
 import transaction
 import wallet
+import constants;
 
 
 ### JUST A BASIC EXAMPLE OF A REST API WITH FLASK
 
-
+thisNode = node.node();
 
 app = Flask(__name__)
 CORS(app)
-blockchain = blockchain.blockchain()
 
 
 #.......................................................................................
 
 
 
-# get all transactions in the blockchain
-
-@app.route('/transactions/get', methods=['GET'])
-def get_transactions():
-    transactions = blockchain.get_transactions();
-
-    response = {'transactions': transactions}
+@app.route('/get_balance', methods=['GET'])
+def get_balance():
+    balance = thisNode.myWallet.balance();
+    response = {'balance': balance};
     return jsonify(response), 200
-
-
 
 # run it once fore every node
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
-
     parser = ArgumentParser()
-    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    parser.add_argument('-a', '--address', default = '127.0.0.1', help='public accessible ip address');
+    parser.add_argument('-p', '--port', default=5001, type=int, help='port to listen on')
     parser.add_argument('-b', '--bootstrap', action='store_true', help='is this the bootstrap node?')
     args = parser.parse_args()
     port = args.port
     isIt = args.bootstrap;
+    address = args.address;
+    thisNode = node.node(address, port, isIt, '127.0.0.1', '5000', constants.NODES);
+    print(type(address));
+    print(address)
     app.run(host='127.0.0.1', port=port)
