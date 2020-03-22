a85c6dc20da0e1816d79da34c2d3f42e80b9f111
Stavros Stavrou <45168559+stavros2@users.noreply.github.com>
2020-03-22 17:35:50 +0200

    Windows Batch files with 3 nodes for local testing
    Run the startServers batch file and then the startCLIs

---
+++ startCLIs.bat
@@ -0,0 +1,3 @@
+start cmd.exe /k "conda activate && python cli.py"
+start cmd.exe /k "conda activate && python cli.py -p 5001"
+start cmd.exe /k "conda activate && python cli.py -p 5002"
---
+++ startServers.bat
@@ -0,0 +1,3 @@
+start cmd.exe /k "conda activate && python rest.py -b" 
+start cmd.exe /k "conda activate && python rest.py -p 5001" 
+start cmd.exe /k "conda activate && python rest.py -p 5002" 
